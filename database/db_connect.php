5ae45bf5ef98be28263de6a2046efddc452bbf3d
Wocked61 <DylanP@csu.fullerton.edu>
2025-11-26 04:02:07 -0800

    added front to backend request and fixed the php connecting function

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,13 +1,16 @@
 <?php 
-$dotenv = parse_ini_file(__DIR__ . '/../.env');
-$host = $dotenv['DB_HOST'];
-$port = $dotenv['DB_PORT'];
-$user = $dotenv['DB_USER'];
-$pass = $dotenv['DB_PASSWORD'];
-$dbname = $dotenv['DB_NAME'];
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function db_connect() {
+    $dotenv = parse_ini_file(__DIR__ . '/../.env');
+    $host = $dotenv['DB_HOST'];
+    $port = $dotenv['DB_PORT'];
+    $user = $dotenv['DB_USER'];
+    $pass = $dotenv['DB_PASSWORD'];
+    $dbname = $dotenv['DB_NAME'];
+
+    $conn = new mysqli($host, $user, $pass, $dbname, $port);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
 }
-return $conn;
 ?>
